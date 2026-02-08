67463382280d1259bacd7fc4d658f7a0c6a5b063
Rodrigo Silva <rodrigo2013.rs33@gmail.com>
2026-02-07 22:45:10 -0300

    primeiro commit curriculo

---
+++ script.js
@@ -0,0 +1,4 @@
+// Exemplo simples para animar o scroll ou exibir uma mensagem de boas-vindas no console
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Currículo de Rodrigo Silva carregado com sucesso!");
+});
